5447f2c856065753b9c7292049f0b0b9042c4843
khalid <khalidcomilla58@gmail.com>
2025-06-18 00:56:04 +0600

    EXMP : derived state example has been added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-
+import { useState } from "react"
 
 function App() {
+  const [price, setPrice] = useState(5)
+  const [quantity, setQuantity] = useState(1)
 
+  const result = price * quantity
 
   return (
     <>
-      <p>Hello world</p>
+      <button 
+        onClick={() => setPrice(price + 1)}
+      > 
+        + price
+      </button> <br/>
+      <button 
+        onClick={() => setQuantity(quantity + 1)}
+      > 
+        + quantity
+      </button>
+      <p>price = {price}</p>
+      <p>quantity = {quantity}</p>
+      <p>result = {result}</p>
     </>
   )
 }
